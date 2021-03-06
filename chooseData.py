f2d86b69257e96eba7ed32db611ef602b9992ea6
Sai Coumar <55699636+saiccoumar@users.noreply.github.com>
2021-03-06 16:44:29 -0600

    model ready to run

--- chooseData.py
+++ chooseData.py
@@ -6,7 +6,7 @@ import shutil
 path = os.path.abspath('')
 print("Path: "+path)
 INPUT_PATH = path
-entries = os.path.join(INPUT_PATH+"/archive/ETFs")
+entries = os.path.join(INPUT_PATH+"/archive/Stocks")
 print(os.listdir(entries))
 # dummy = pd.read_csv(os.path.join(entries+'/iba.us.txt'), engine='c')
 # print(dummy.iloc[-1]['Volume'])
@@ -15,18 +15,20 @@ for x in os.listdir(entries):
     print("entry:"+x)
     filePath = os.path.join(entries+ "/"+x)
     print(filePath)
-    if filePath != entries+"/.DS_Store":
-        if os.stat(filePath).st_size > 0:
-            df_ge = pd.read_csv(filePath, engine='c')
 
-            print("value: ")
-            if df_ge.iloc[-1]['Volume'] == '':
-                print("empty")
-            else:
-                date = df_ge.iloc[-1]['Volume']
-            # year = int(date[0:4])
-                print(date)
-            if (date>12500000):
-                    shutil.move(filePath, os.path.join(INPUT_PATH+ "/ETFInput"))
+    if x == ".DS_Store":
+        print("yes")
+    #     if os.stat(filePath).st_size > 0:
+            # df_ge = pd.read_csv(filePath, engine='c')
+
+            # print("value: ")
+            # if df_ge.iloc[-1]['Volume'] == '':
+            #     print("empty")
+            # else:
+            #     date = df_ge.iloc[-1]['Volume']
+            # # year = int(date[0:4])
+            #     print(date)
+            # if (date>12500000):
+            #         shutil.move(filePath, os.path.join(INPUT_PATH+ "/ETFInput"))
 
 print("fin")
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import logging
 
 params = {
     "batch_size": 20,  # 20<16<10, 25 was a bust
-    "epochs": 100,
+    "epochs": 5,
     "lr": 0.00010000,
     "time_steps": 60
 }
@@ -28,7 +28,7 @@ iter_changes = "dropout_layers_0.4_0.4"
 path = os.path.abspath('')
 print("Path: "+path)
 INPUT_PATH = path
-OUTPUT_PATH = path+"/outputs/lstm_best_7-3-19_12AM/"+iter_changes
+OUTPUT_PATH = path+"/outputs"
 TIME_STEPS = params["time_steps"]
 BATCH_SIZE = params["batch_size"]
 stime = time.time()
@@ -69,38 +69,6 @@ def build_timeseries(mat, y_col_index):
 #           print(i,"-->", x[i,-1,:], y[i])
     print("length of time-series i/o",x.shape,y.shape)
     return x, y
-
-stime = time.time()
-print(os.listdir(INPUT_PATH))
-df_ge = pd.read_csv(os.path.join(INPUT_PATH, "ge.us.txt"), engine='python')
-print(df_ge.shape)
-print(df_ge.columns)
-print(df_ge.head(5))
-tqdm_notebook.pandas('Processing...')
-# df_ge = process_dataframe(df_ge)
-print(df_ge.dtypes)
-train_cols = ["Open","High","Low","Close","Volume"]
-df_train, df_test = train_test_split(df_ge, train_size=0.8, test_size=0.2, shuffle=False)
-print("Train--Test size", len(df_train), len(df_test))
-
-x = df_train.loc[:,train_cols].values
-min_max_scaler = MinMaxScaler()
-x_train = min_max_scaler.fit_transform(x)
-x_test = min_max_scaler.transform(df_test.loc[:,train_cols])
-
-print("Deleting unused dataframes of total size(KB)",(sys.getsizeof(df_ge)+sys.getsizeof(df_train)+sys.getsizeof(df_test))//1024)
-
-del df_ge
-del df_test
-del df_train
-del x
-
-print("Are any NaNs present in train/test matrices?",np.isnan(x_train).any(), np.isnan(x_train).any())
-x_t, y_t = build_timeseries(x_train, 3)
-x_t = trim_dataset(x_t, BATCH_SIZE)
-y_t = trim_dataset(y_t, BATCH_SIZE)
-print("Batch trimmed size",x_t.shape, y_t.shape)
-
 def create_model():
     lstm_model = Sequential()
     # (batch_size, timesteps, data_dim)
@@ -116,106 +84,157 @@ def create_model():
     # optimizer = optimizers.SGD(lr=0.000001, decay=1e-6, momentum=0.9, nesterov=True)
     lstm_model.compile(loss='mean_squared_error', optimizer=optimizer)
     return lstm_model
+etf =  os.path.join(INPUT_PATH+"/archive/ETFs")
+stock = entries = os.path.join(INPUT_PATH+"/archive/Stocks")
+for data in os.listdir(etf):
+    if data != ".DS_Store":
+        print(data)
+        ticker = data.replace(".us.txt","")
+        # print(ticker)
+        stime = time.time()
+        # print(os.listdir(INPUT_PATH))
+        df_ge = pd.read_csv(os.path.join(etf, data), engine='python')
+        # print(df_ge.shape)
+        # print(df_ge.columns)
+        # print(df_ge.head(5))
+        tqdm_notebook.pandas('Processing...')
+    # df_ge = process_dataframe(df_ge)
+        # print(df_ge.dtypes)
+        train_cols = ["Open","High","Low","Close","Volume"]
+        df_train, df_test = train_test_split(df_ge, train_size=0.8, test_size=0.2, shuffle=False)
+        # print("Train--Test size", len(df_train), len(df_test))
+
+        x = df_train.loc[:,train_cols].values
+        min_max_scaler = MinMaxScaler()
+        x_train = min_max_scaler.fit_transform(x)
+        x_test = min_max_scaler.transform(df_test.loc[:,train_cols])
+
+        # print("Deleting unused dataframes of total size(KB)",(sys.getsizeof(df_ge)+sys.getsizeof(df_train)+sys.getsizeof(df_test))//1024)
+
+        del df_ge
+        del df_test
+        del df_train
+        del x
+
+        # print("Are any NaNs present in train/test matrices?",np.isnan(x_train).any(), np.isnan(x_train).any())
+        x_t, y_t = build_timeseries(x_train, 3)
+        x_t = trim_dataset(x_t, BATCH_SIZE)
+        y_t = trim_dataset(y_t, BATCH_SIZE)
+        # print("Batch trimmed size",x_t.shape, y_t.shape)
+
+        model = None
+        # try:
+        #     model = pickle.load(open("lstm_model", 'rb'))
+        #     # print("Loaded saved model...")
+        # except FileNotFoundError:
+        #     # print("Model not found")
 
 
-model = None
-try:
-    model = pickle.load(open("lstm_model", 'rb'))
-    print("Loaded saved model...")
-except FileNotFoundError:
-    print("Model not found")
+        x_temp, y_temp = build_timeseries(x_test, 3)
+        x_val, x_test_t = np.split(trim_dataset(x_temp, BATCH_SIZE),2)
+        y_val, y_test_t = np.split(trim_dataset(y_temp, BATCH_SIZE),2)
 
+        # print("Test size", x_test_t.shape, y_test_t.shape, x_val.shape, y_val.shape)
 
-x_temp, y_temp = build_timeseries(x_test, 3)
-x_val, x_test_t = np.split(trim_dataset(x_temp, BATCH_SIZE),2)
-y_val, y_test_t = np.split(trim_dataset(y_temp, BATCH_SIZE),2)
-
-print("Test size", x_test_t.shape, y_test_t.shape, x_val.shape, y_val.shape)
-
-is_update_model = True
-if model is None or is_update_model:
-    from keras import backend as K
-    print("Building model...")
-#     print("checking if GPU available", K.tensorflow_backend._get_available_gpus())
-    model = create_model()
+        is_update_model = True
+        if model is None or is_update_model:
+            from keras import backend as K
+            # print("Building model...")
+    # print("checking if GPU available", K.tensorflow_backend._get_available_gpus())
+            model = create_model()
     
-    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1,
+            es = EarlyStopping(monitor='val_loss', mode='min', verbose=1,
                        patience=40, min_delta=0.0001)
     
-    mcp = ModelCheckpoint(os.path.join(OUTPUT_PATH,
-                          "best_model.h5"), monitor='val_loss', verbose=1,
+            mcp = ModelCheckpoint(os.path.join(OUTPUT_PATH,
+                          ticker+".h5"), monitor='val_loss', verbose=1,
                           save_best_only=True, save_weights_only=False, mode='min', period=1)
 
     # Not used here. But leaving it here as a reminder for future
-    r_lr_plat = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=30, 
+            r_lr_plat = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=30, 
                                   verbose=0, mode='auto', min_delta=0.0001, cooldown=0, min_lr=0)
     
-    csv_logger = CSVLogger(os.path.join(OUTPUT_PATH, 'training_log_' + time.ctime().replace(" ","_") + '.log'), append=True)
+    # csv_logger = CSVLogger(os.path.join(OUTPUT_PATH, 'training_log_' + time.ctime().replace(" ","_") + '.log'), append=True)
     
-    history = model.fit(x_t, y_t, epochs=params["epochs"], verbose=2, batch_size=BATCH_SIZE,
+            history = model.fit(x_t, y_t, epochs=params["epochs"], verbose=2, batch_size=BATCH_SIZE,
                         shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE),
                         trim_dataset(y_val, BATCH_SIZE)), callbacks=[es, r_lr_plat, mcp])
     
-    print("saving model...")
+            print("saving etf model...")
+for data in os.listdir(stock):
+    if data != ".DS_Store":
+        print(data)
+        ticker = data.replace(".us.txt","")
+        print(ticker)
+        stime = time.time()
+        print(os.listdir(INPUT_PATH))
+        df_ge = pd.read_csv(os.path.join(etf, data), engine='python')
+        print(df_ge.shape)
+        print(df_ge.columns)
+        print(df_ge.head(5))
+        tqdm_notebook.pandas('Processing...')
+    # df_ge = process_dataframe(df_ge)
+        print(df_ge.dtypes)
+        train_cols = ["Open","High","Low","Close","Volume"]
+        df_train, df_test = train_test_split(df_ge, train_size=0.8, test_size=0.2, shuffle=False)
+        print("Train--Test size", len(df_train), len(df_test))
+
+        x = df_train.loc[:,train_cols].values
+        min_max_scaler = MinMaxScaler()
+        x_train = min_max_scaler.fit_transform(x)
+        x_test = min_max_scaler.transform(df_test.loc[:,train_cols])
+
+        print("Deleting unused dataframes of total size(KB)",(sys.getsizeof(df_ge)+sys.getsizeof(df_train)+sys.getsizeof(df_test))//1024)
+
+        del df_ge
+        del df_test
+        del df_train
+        del x
+
+        print("Are any NaNs present in train/test matrices?",np.isnan(x_train).any(), np.isnan(x_train).any())
+        x_t, y_t = build_timeseries(x_train, 3)
+        x_t = trim_dataset(x_t, BATCH_SIZE)
+        y_t = trim_dataset(y_t, BATCH_SIZE)
+        print("Batch trimmed size",x_t.shape, y_t.shape)
+
+        model = None
+        try:
+            model = pickle.load(open("lstm_model", 'rb'))
+            print("Loaded saved model...")
+        except FileNotFoundError:
+            print("Model not found")
+
+
+        x_temp, y_temp = build_timeseries(x_test, 3)
+        x_val, x_test_t = np.split(trim_dataset(x_temp, BATCH_SIZE),2)
+        y_val, y_test_t = np.split(trim_dataset(y_temp, BATCH_SIZE),2)
+
+        print("Test size", x_test_t.shape, y_test_t.shape, x_val.shape, y_val.shape)
+
+        is_update_model = True
+        if model is None or is_update_model:
+            from keras import backend as K
+            print("Building model...")
+    #  print("checking if GPU available", K.tensorflow_backend._get_available_gpus())
+            model = create_model()
+    
+            es = EarlyStopping(monitor='val_loss', mode='min', verbose=1,
+                       patience=40, min_delta=0.0001)
+    
+            mcp = ModelCheckpoint(os.path.join(OUTPUT_PATH,
+                          ticker+".h5"), monitor='val_loss', verbose=1,
+                          save_best_only=True, save_weights_only=False, mode='min', period=1)
+
+    # Not used here. But leaving it here as a reminder for future
+            r_lr_plat = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=30, 
+                                  verbose=0, mode='auto', min_delta=0.0001, cooldown=0, min_lr=0)
+    
+    # csv_logger = CSVLogger(os.path.join(OUTPUT_PATH, 'training_log_' + time.ctime().replace(" ","_") + '.log'), append=True)
+    
+            history = model.fit(x_t, y_t, epochs=params["epochs"], verbose=2, batch_size=BATCH_SIZE,
+                        shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE),
+                        trim_dataset(y_val, BATCH_SIZE)), callbacks=[es, r_lr_plat, mcp])
+    
+            print("saving stock model...")
 #     pickle.dump(model, open("lstm_model", "wb"))
 
-# model.evaluate(x_test_t, y_test_t, batch_size=BATCH_SIZE
-y_pred = model.predict(trim_dataset(x_test_t, BATCH_SIZE), batch_size=BATCH_SIZE)
-y_pred = y_pred.flatten()
-y_test_t = trim_dataset(y_test_t, BATCH_SIZE)
-error = mean_squared_error(y_test_t, y_pred)
-print("Error is", error, y_pred.shape, y_test_t.shape)
-print(y_pred[0:15])
-print(y_test_t[0:15])
-
-# convert the predicted value to range of real data
-y_pred_org = (y_pred * min_max_scaler.data_range_[3]) + min_max_scaler.data_min_[3]
-# min_max_scaler.inverse_transform(y_pred)
-y_test_t_org = (y_test_t * min_max_scaler.data_range_[3]) + min_max_scaler.data_min_[3]
-# min_max_scaler.inverse_transform(y_test_t)
-print(y_pred_org[0:15])
-print(y_test_t_org[0:15])
-
-
-
-# disable for mass model 
-# Visualize the training data
-from matplotlib import pyplot as plt
-plt.figure()
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-#plt.show()
-plt.savefig(os.path.join(OUTPUT_PATH, 'train_vis_BS_'+str(BATCH_SIZE)+"_"+time.ctime()+'.png'))
-
-# load the saved best model from above
-saved_model = load_model(os.path.join(OUTPUT_PATH, 'best_model.h5')) # , "lstm_best_7-3-19_12AM",
-print(saved_model)
-
-y_pred = saved_model.predict(trim_dataset(x_test_t, BATCH_SIZE), batch_size=BATCH_SIZE)
-y_pred = y_pred.flatten()
-y_test_t = trim_dataset(y_test_t, BATCH_SIZE)
-error = mean_squared_error(y_test_t, y_pred)
-print("Error is", error, y_pred.shape, y_test_t.shape)
-print(y_pred[0:15])
-print(y_test_t[0:15])
-y_pred_org = (y_pred * min_max_scaler.data_range_[3]) + min_max_scaler.data_min_[3] # min_max_scaler.inverse_transform(y_pred)
-y_test_t_org = (y_test_t * min_max_scaler.data_range_[3]) + min_max_scaler.data_min_[3] # min_max_scaler.inverse_transform(y_test_t)
-print(y_pred_org[0:15])
-print(y_test_t_org[0:15])
-
-# Visualize the prediction
-from matplotlib import pyplot as plt
-plt.figure()
-plt.plot(y_pred_org)
-plt.plot(y_test_t_org)
-plt.title('Prediction vs Real Stock Price')
-plt.ylabel('Price')
-plt.xlabel('Days')
-plt.legend(['Prediction', 'Real'], loc='upper left')
-#plt.show()
-plt.savefig(os.path.join(OUTPUT_PATH, 'pred_vs_real_BS'+str(BATCH_SIZE)+"_"+time.ctime()+'.png'))
-print_time("program completed ", stime)
